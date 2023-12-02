14bcd4ab2d466ef9589e103b704ae13defbd690b
Divyaranjan Pradhan <divyaranjan619@gmail.com>
2023-12-02 23:01:18 +0530

    Food Delivery update

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,3 @@
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=16.922464&lng=81.794091&restaurantId=";
+export const MENU_API = "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=16.922464&lng=81.794091&restaurantId=";
